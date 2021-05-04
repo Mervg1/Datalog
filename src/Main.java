5367586ddd7b4065ea3d0c10405af00493f29e37
Mercedes Vázquez García <a01655224@itesm.mx>
2021-05-03 20:00:58 -0500

    arreglos en gramática

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+
+
+            CharStream input = CharStreams.fromStream(System.in);
+            DatalogLexer lexer = new DatalogLexer(input);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            DatalogParser parser = new DatalogParser(tokens);
+            ParseTree tree = parser.program();
+            System.out.println("\n");
+            System.out.println(tree.toStringTree(parser));
+            /*Visitador eval = new Visitador();
+            HashMap<Integer, HashMap<Character, Set<Integer>>> n = eval.visit(tree);
+
+            TablaTransicion tabla = getTablaTrancision(n,eval);
+            System.out.println(tabla);*/
+
+
+        /*
+        ParseTreeWalker walker = new ParseTreeWalker();
+        MiListener listener = new MiListener();
+        walker.walk(listener, tree);
+        */
+        }
+        catch(Exception e) {
+            System.out.println("Error"); }
+    }
+}
